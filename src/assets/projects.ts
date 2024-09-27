87f20f5e971ea14a9dc81543de6916ac0177e845
Kabrax01 <rogalski.arnold@gmail.com>
2024-09-27 15:14:28 +0200

    feat: added projects section

---
+++ src/assets/projects.ts
@@ -0,0 +1,39 @@
+interface projectsType {
+    name: string;
+    siteUrl: string;
+    githubUrl: string;
+    img: string;
+    description: string;
+    id: number;
+}
+const projects: projectsType[] = [
+    {
+        name: "Chatrix",
+        siteUrl: "https://kabrax01.github.io/Chatrix/",
+        githubUrl: "https://github.com/Kabrax01/Chatrix",
+        img: "../../public/chatrix.webp",
+        description:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci earum quae architecto labore et excepturi! Totam optio provident non ipsum!",
+        id: 1,
+    },
+    {
+        name: "Chatrix",
+        siteUrl: "https://kabrax01.github.io/Chatrix/",
+        githubUrl: "https://github.com/Kabrax01/Chatrix",
+        img: "../../public/chatrix.webp",
+        description:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci earum quae architecto labore et excepturi! Totam optio provident non ipsum!",
+        id: 2,
+    },
+    {
+        name: "Chatrix",
+        siteUrl: "https://kabrax01.github.io/Chatrix/",
+        githubUrl: "https://github.com/Kabrax01/Chatrix",
+        img: "../../public/chatrix.webp",
+        description:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci earum quae architecto labore et excepturi! Totam optio provident non ipsum!",
+        id: 3,
+    },
+];
+
+export default projects;
--- src/pages/projects/Projects.tsx
+++ src/pages/projects/Projects.tsx
@@ -1,5 +1,24 @@
+import "./projects.scss";
+import projects from "../../assets/projects";
+
 function Projects() {
-    return <div>Projects</div>;
+    return (
+        <div className="projects">
+            {projects.map((project) => {
+                return (
+                    <div className="project" key={project.id}>
+                        <h2>{project.name}</h2>
+                        <div className="project__img">
+                            <img src={project.img} alt="" />
+                        </div>
+                        <div className="project__desc">
+                            <p>{project.description}</p>
+                        </div>
+                    </div>
+                );
+            })}
+        </div>
+    );
 }
 
 export default Projects;
