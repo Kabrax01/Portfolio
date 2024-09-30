59332d93de16457d3aa323f0e0141b2af0861245
Kabrax01 <rogalski.arnold@gmail.com>
2024-09-30 18:52:18 +0200

    feat: added responsive images

--- src/assets/projects.ts
+++ src/assets/projects.ts
@@ -3,6 +3,8 @@ export interface ProjectsType {
     siteUrl: string;
     githubUrl: string;
     img: string;
+    tabletsImg?: string;
+    mobileImg?: string;
     description: string;
     id: number;
 }
@@ -11,7 +13,9 @@ const projects: ProjectsType[] = [
         name: "Chatrix",
         siteUrl: "https://kabrax01.github.io/Chatrix/",
         githubUrl: "https://github.com/Kabrax01/Chatrix",
-        img: "../../public/chatrix.webp",
+        img: "../../public/projects/chatrix_large",
+        tabletsImg: "../../public/projects/chatrix_medium",
+        mobileImg: "../../public/projects/chatrix_mobile",
         description:
             "Chatrix is a real-time communication application that allows users to send text and multimedia messages in private chats. The app offers features such as registration, login, user profile management (changing name, profile picture), and searching for other users to start a conversation. It provides a simple interface with a chat list view, enabling users to easily switch between conversations, as well as intuitive messaging and multimedia sharing.",
         id: 1,
@@ -20,7 +24,9 @@ const projects: ProjectsType[] = [
         name: "React Quiz",
         siteUrl: "https://kabrax01.github.io/React_Quiz/",
         githubUrl: "https://github.com/Kabrax01/React_Quiz",
-        img: "../../public/react_quiz.webp",
+        img: "../../public/projects/react_quiz_large",
+        tabletsImg: "../../public/projects/react_quiz_medium",
+        mobileImg: "../../public/projects/react_quiz_mobile",
         description:
             "Quiz app with time restrictions, score counter, and minimalistic interface for testing knowledge about React framework. It uses firebase to store questions, enabling future expanding for multi topics selection",
         id: 2,
@@ -29,18 +35,22 @@ const projects: ProjectsType[] = [
         name: "Tomasz Gruchacz Racing",
         siteUrl: "https://kabrax01.github.io/Tomasz_Gruchacz_Racing/",
         githubUrl: "https://github.com/Kabrax01/Tomasz_Gruchacz_Racing",
-        img: "../../public/TGR.webp",
+        img: "../../public/projects/TGR_large",
+        tabletsImg: "../../public/projects/TGR_medium",
+        mobileImg: "../../public/projects/TGR_mobile",
         description:
-            "Landing page for Tomasz Gruchacz - motorcycle racer. With on-scroll animations and contact form. Written in vanilla JavaScript",
+            "Landing page for Tomasz Gruchacz - motorcycle racer. With on-scroll animations, auto image slider and contact form. Written in vanilla JavaScript",
         id: 3,
     },
     {
         name: "Notes",
         siteUrl: "https://kabrax01.github.io/Notes/",
         githubUrl: "https://github.com/Kabrax01/Notes",
-        img: "../../public/notes.webp",
+        img: "../../public/projects/notes_large",
+        tabletsImg: "../../public/projects/notes_medium",
+        mobileImg: "../../public/projects/notes_mobile",
         description:
-            "Landing page for Tomasz Gruchacz - motorcycle racer. With on-scroll animations and contact form. Written in vanilla JavaScript",
+            "Notes is simple application allowing users to add, delete and edit notes. It stores data in browser storage. Builded without using context API to face the problem of props-drilling",
         id: 4,
     },
 ];
