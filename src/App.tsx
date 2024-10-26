61aecd19e1b4ace3539295774b4decae3deee7c3
Kabrax01 <rogalski.arnold@gmail.com>
2024-10-26 14:17:51 +0200

    feat: added fix for gh pages for 404 error

--- src/App.tsx
+++ src/App.tsx
@@ -5,16 +5,19 @@ import Main from "./pages/main/Main";
 import ProjectsSlider from "./pages/projects/ProjectsSlider";
 import Contact from "./pages/contact/Contact";
 
-const router = createBrowserRouter([
-    {
-        element: <AppLayout />,
-        children: [
-            { path: "/Portfolio/", element: <Main /> },
-            { path: "/Portfolio/projects", element: <ProjectsSlider /> },
-            { path: "/Portfolio/contact", element: <Contact /> },
-        ],
-    },
-]);
+const router = createBrowserRouter(
+    [
+        {
+            element: <AppLayout />,
+            children: [
+                { path: "/Portfolio/", element: <Main /> },
+                { path: "/Portfolio/projects", element: <ProjectsSlider /> },
+                { path: "/Portfolio/contact", element: <Contact /> },
+            ],
+        },
+    ],
+    { basename: "/Portfolio" }
+);
 
 function App() {
     return <RouterProvider router={router} />;
