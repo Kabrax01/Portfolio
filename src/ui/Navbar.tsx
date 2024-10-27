cd7abf247c5f2c28a1c248c2ee8755cdffb2423a
Kabrax01 <rogalski.arnold@gmail.com>
2024-10-27 13:32:11 +0100

    Revert "feat: added absolute path to img"
    This reverts commit db586aa3c2be3707de241fbed754564df5c464b1.

--- src/ui/Navbar.tsx
+++ src/ui/Navbar.tsx
@@ -13,10 +13,7 @@ function Navbar() {
         <div className="navbar">
             <div className="navbar__container">
                 <Link to="/Portfolio/" className="logo_link">
-                    <img
-                        src={`${import.meta.env.BASE_URL}./logo.svg`}
-                        alt="logo image"
-                    />
+                    <img src="./logo.svg" alt="logo image" />
                 </Link>
 
                 <Hamburger
